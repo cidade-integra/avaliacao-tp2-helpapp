aada447af04578e648199e2d16beb6a715c55884
miguelmorandini <miguelmorandini2005@gmail.com>
2025-06-23 20:57:42 -0300

    Endpoints de create implementado

---
+++ StockApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.DTOs;
+using StockApp.Application.Interfaces;
+using StockApp.Domain.Entities;
+
+namespace StockApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductDTO>> Create(ProductDTO productDto)
+        {
+            if (productDto == null)
+            {
+                return BadRequest("Invalid data.");
+            }
+
+            var product = await _productService.Add(productDto);
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+        [HttpGet("{id}", Name = "GetById")]
+        public async Task<ActionResult<ProductDTO>> GetById(int id)
+        {
+            var product = await _productService.GetProductById(id);
+            if(product == null)
+            {
+                return NotFound("Product not found");
+            }
+            return Ok(product);
+        }
+    }
+}
