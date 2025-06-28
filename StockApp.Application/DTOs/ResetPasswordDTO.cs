dd5a6cf2c1b074ccdf3077fa294bae213afa6946
Victor Testi <victortesti06@gmail.com>
2025-06-27 22:10:37 -0300

    feat: Implementação do Sistema de Recuperação de Senha

---
+++ StockApp.Application/DTOs/ResetPasswordDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.DTOs
+{
+    public class ResetPasswordDTO
+    {
+        public string Username { get; set; }
+        public string Token {  get; set; }
+        public string NewPassword { get; set; }        
+    }
+}
--- StockApp.Domain/Entities/User.cs
+++ StockApp.Domain/Entities/User.cs
@@ -11,6 +11,8 @@ namespace StockApp.Domain.Entities
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string Role { get; set; } = "User";
+
+        public string PasswordResetToken { get; set; }
         #endregion
 
         #region Construtores
